73db43f0af35325b10ccff2c7dc7abed1b355673
Yeonwoo <yeonwoobae2003@gmail.com>
2025-04-01 18:58:38 -0700

    querySelector allows the selection of one specific element and you can do whatever you want with it, like change it

--- Intro_to_DOM/script.js
+++ Intro_to_DOM/script.js
@@ -1,3 +1,11 @@
 let titleElement = document.getElementById("title");
+let heading = document.querySelector("mainHeading");
+let paragraph = document.querySelector(".description");
+paragraph = "The text is different now.";
+let button = document.querySelector("changeText");
+
 
 console.log(titleElement);
+console.log(heading);
+console.log(paragraph);
+console.log(button);
