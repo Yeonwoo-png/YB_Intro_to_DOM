a71848eea277a7a88b92793d8538ff9e84c644f1
Yeonwoo <yeonwoobae2003@gmail.com>
2025-04-06 22:33:33 -0700

    document.querySelector doesn't actually change the text in the webpage, it instead replaces the paragraph variable itself with what we replaced it with the line after we initialized the paragraph variable. It selects the first element with the class you are specifying in the string

--- scripts/script.js
+++ scripts/script.js
@@ -3,14 +3,12 @@ let titleElement = document.getElementById("title");
 
 console.log(titleElement);
 
-
+//document.querySelector Excercise
 let heading = document.querySelector("mainHeading");
 let paragraph = document.querySelector(".description");
 paragraph = "The text is different now.";
 let button = document.querySelector("changeText");
 
-
-console.log(titleElement);
 console.log(heading);
 console.log(paragraph);
 console.log(button);
